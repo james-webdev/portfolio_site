316f95600d7d1685eb61516f338f6de4928b3d4c
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-06-10 19:06:20 +0100

    rewording the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,14 +16,14 @@ const About = () => {
         <div className="h-96 ml-8 mr-8 flex flex-col mt-40 mb-80 md:mt-5 max-w-3xl text-center justify-center items-center">
           <p className="text-md sm:text-xl">
             At the beginning of 2019 I was stuck in a career I
-            wasn't enjoying and faced with the failure of a company I had put my
+            wasn't enjoying and faced with the closure of a company I had put my
             all into for several years. I was looking for a way forward.{" "}
           </p>
           <p className="text-md sm:text-xl mt-5">
-            As if from above the idea of development came to me. I was able to
+            A succesion of synchronous events led me to development. I was able to
             apply for and get on a course entirely funded by the French state
             and get funding for 2 years to change career. After the first PHP
-            focused web and mobile web development course I began a second
+            focused web and mobile web development course I began a second fully funded
             course in Full Stack JavaScript development.
           </p>
           <p className="text-md sm:text-xl mt-5">
@@ -39,7 +39,7 @@ const About = () => {
           </p>
 
           <p className="text-md sm:text-xl mt-5">
-            After 14 years in Paris I am now back in the UK and looking for my
+            After 14 years in Paris I am now back in Surrey, where I grew up, and looking for my
             next role. Outside of work I enjoy road biking and reading.
             Last year I wrote a short dystopian novel set in the 22nd century.
           </p>
