b0e8ab6f3cd62bf98281c1357660601764dcb623
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-06-21 20:42:20 +0100

    animate.css added and projects to guardian and artbook pages

--- src/components/TextLooper.js
+++ src/components/TextLooper.js
@@ -34,9 +34,9 @@ const TextLooper = () => {
       <div className="h-60 text-2xl p-1 font-bold flex flex-col sm:flex-row items-center justify-center">
         <h2 className="font-semibold">Some tools I use:&nbsp;</h2>
         <TextLoop className="z-0 bgSiteGreen p-1 text-2xl text-white rounded-lg">
-          <p className="ml-2 z-0 mt-2">React.js.</p>
+          <p className="ml-2 z-0 mt-2">ReactJS.</p>
           <p className="ml-2 z-0 mt-2">JavaScript ES6.</p>
-          <p className="ml-2 z-0 mt-2">Gatsby.js.</p>
+          <p className="ml-2 z-0 mt-2">GatsbyJS.</p>
           <p className="ml-2 z-0 mt-2">CSS animations.</p>
           <p className="ml-2 z-0 mt-2">Responsive design.</p>
           <p className="ml-2 z-0 mt-2">CSS Grid.</p>
