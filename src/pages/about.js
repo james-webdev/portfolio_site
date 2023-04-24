82b6eed2102c83fe6ed935d098c274d99fa2d8cd
James Roe <58564719+james-webdev@users.noreply.github.com>
2023-04-24 15:55:49 -0500

    update about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby";
 import Layout from "../components/Layout";
 import TextLooper from "../components/TextLooper";
 import Me from "../images/jim.png";
-import Me2 from "../images/Me2.png";
+import Me2 from "../images/Jameslinkedinpic.png";
 
 const About = () => {
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center">
         <div className="flex justify-center items-center mt-40 sm:mt-28 mb-20">
-          <img className="w-56 rounded-lg shadow-lg" src={Me2} alt="" />
+          <img className="w-72 rounded-lg shadow-lg" src={Me2} alt="" />
         </div>
 
         <div className="h-96 ml-8 mr-8 flex flex-col mt-40 mb-80 md:mt-5 max-w-3xl text-center justify-center items-center">
@@ -32,21 +32,18 @@ const About = () => {
             JavaScript development.
           </p>
           <p className="text-md sm:text-xl mt-5">
-            I have been coding nearly every day for 2 years now and have worked
-            on a range of
-            <Link className="textSiteGreen" to="/projects">
-              {" "}
-              projects{" "}
-            </Link>
-            in different stacks and for different companies. I find development
+            I have been coding nearly every day for 4 years now. The past year and half I have been working at  <a
+              className="textSiteGreen"
+              href="https://shape.works"
+            >
+              Shape Works
+            </a> helping them build websites with React, SASS, PHP and JavaScript. I find development
             to be the perfect balance of creativity, which I value very highly,
             and problem solving, which keeps my analytical mind busy.
           </p>
 
           <p className="text-md sm:text-xl mt-5">
-            After 14 years in Paris I am now back in the UK and looking for my
-            next challenge. Outside of work I enjoy road biking and reading.
-            Last year I wrote a short dystopian novel set in the 22nd century.
+             Outside of work I enjoy road biking, swimming and reading.
           </p>
         </div>
       </div>
